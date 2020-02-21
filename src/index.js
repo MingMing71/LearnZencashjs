554442d82bc044f9356c5c57dde7922fe6f5ce6b
Ming Ming <phamquangminhex@gmail.com>
2020-02-21 10:40:34 +0700

    Use 3 HD wallet to generate multisig address

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import zencashjs from 'zencashjs'
 import hdkey from 'hdkey'
 const bip39 = require('bip39')
+const bip32 = require('bip32')
 
-function sendZen(zen: Number = 0, address: String = "null") {
+function sendZen(zen: Number = 0, senderAddr: String, recipientAddr: String) {
   var utxos = null;
 
   const Http = new XMLHttpRequest();
-  const url = 'https://explorer-testnet.zensystem.io/api/addr/' + zAddr + '/utxo'
+  const url = 'https://explorer-testnet.zensystem.io/api/addr/' + senderAddr + '/utxo'
   Http.open("GET", url);
   Http.send();
 
@@ -18,14 +19,14 @@ function sendZen(zen: Number = 0, address: String = "null") {
       var data = JSON.parse(Http.response);
       utxos = data;
 
-      createHexRawTx(utxos, zen, address);
+      createHexRawTx(utxos, zen, senderAddr, recipientAddr);
       return;
     }
   }
 }
 
 
-function createHexRawTx(utxos, zen: Number = 0, address: String = "null") {
+function createHexRawTx(utxos, zen: Number = 0, senderAddr: String, recipientAddr: String) {
   const lastUTXO = utxos.find(function (transaction) {
     return transaction.height;
   });
@@ -58,21 +59,19 @@ function createHexRawTx(utxos, zen: Number = 0, address: String = "null") {
     if (Http.readyState == 4 && Http.status == 200) {
       var data = JSON.parse(Http.response);
       bip115BlockHash = data.blockHash;
-      console.log(bip115BlockHeight, bip115BlockHash, lastUTXO.txid, lastUTXO.vout, zAddr)
+
       const txobj = zencashjs.transaction.createRawTx(
         [{
           txid: lastUTXO.txid, vout: lastUTXO.vout,
           scriptPubKey: ''
         }],
         [
-          { address: address, satoshis: amountToSend },
-          { address: zAddr, satoshis: balance - totalChange },
+          { address: recipientAddr, satoshis: amountToSend },
+          { address: senderAddr, satoshis: balance - totalChange },
         ],
         bip115BlockHeight,
         bip115BlockHash
       )
-
-      console.log(txobj)
 
       var sig1 = zencashjs.transaction.multiSign(txobj, 0, privKeys[0], redeemScript)
 
@@ -108,21 +107,38 @@ function sendRawTx(rawTx: String) {
   http.send(params);
 }
 
-var phrase = [
-  'sea brown please gold erosion utility table thumb social',
-  'immense globe need dawn people labor shy stomach ostrich',
-  'capable under wrap episode giant upset brave illness reward'
-]
+var hdnode;
 var privKeys = [];
+var pubKeys;
+var redeemScript;
+var pubKeyHash;
+var zAddr;
 
-phrase.forEach((phrase) => {
-  privKeys.push(zencashjs.address.mkPrivKey(phrase));
+const mnemonic = [
+  'capable under wrap episode giant upset brave illness reward chicken useless above',
+  'fatal situate measure sound cradle ancient barely resist avoid inform hunt soft',
+  'ceiling box blast satoshi door captain soul reveal throw shock blossom match'
+];
+const seed = mnemonic.map((each) => bip39.mnemonicToSeed(each));
+
+var hdmasternode = [];
+Promise.all(seed).then((response) => {
+  for (var i = 0; i < response.length; i++) {
+    // const root = hdkey.fromMasterSeed(response[i]);
+    // hdmasternode.push(root);
+    // const addrNode = root.derive("m"); //line 1
+    // privKeys.push(addrNode._privateKey.toString('hex'));
+    const hdnode = bip32.fromSeed(response[i]);
+    const derivedNode = hdnode.derive(0);
+    privKeys.push(derivedNode.privateKey.toString('hex'));
+  }
+
+  pubKeys = privKeys.map((x) => zencashjs.address.privKeyToPubKey(x, true))
+
+  redeemScript = zencashjs.address.mkMultiSigRedeemScript(pubKeys, 2, 3)
+  pubKeyHash = zencashjs.config.testnet.pubKeyHash
+  zAddr = zencashjs.address.multiSigRSToAddress(redeemScript, pubKeyHash)
+  console.log(zAddr);
+
+  sendZen(0.001, zAddr, 'ztqxUEzHwpxwSjKHm2AsFAxdbBbLZiYWVqX');
 });
-
-var pubKeys = privKeys.map((x) => zencashjs.address.privKeyToPubKey(x, true))
-
-var redeemScript = zencashjs.address.mkMultiSigRedeemScript(pubKeys, 2, 3)
-var pubKeyHash = zencashjs.config.testnet.pubKeyHash
-var zAddr = zencashjs.address.multiSigRSToAddress(redeemScript, pubKeyHash)
-
-sendZen(0.01, 'ztqxUEzHwpxwSjKHm2AsFAxdbBbLZiYWVqX');
